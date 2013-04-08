1fe1c655acc3e0e56f177dcd89ed40ef9332cce3
MasterBlaster <roribib@gmail.com>
2013-04-08 20:17:16 +0200

    Finished STM lifecycle... For now it supports :clockwise and random behaviour

--- src/dreamcatcher/core.clj
+++ src/dreamcatcher/core.clj
@@ -62,13 +62,6 @@
 (defn get-stm [instance]
   (if (map? instance) @(:stm instance) @(:stm @instance)))
 
-(defn get-reachable-states 
-  "Returns reachable states from positon of instance
-  wihtin state machine."
-  [instance]
-  (keys (get-transitions instance (get-stm instance))))
-
-
 (defn valid-transition? 
   "Computes if transition from-state to to-state is valid.
   If there is any type of validator function will try to
@@ -100,6 +93,15 @@
 (defn get-state [instance]
   (if (map? instance) (:state instance) (:state @instance)))
 
+(defn get-reachable-states 
+  "Returns reachable states from positon of instance
+  wihtin state machine."
+  [instance]
+  (keys (get-transitions (get-stm instance) (get-state instance))))
+
+(defn reset-state! [instance new-state]
+  (if (map? instance) (assoc instance :state new-state) (swap! instance assoc :state new-state))) 
+
 (defn get-data [instance]
   (if (map? instance) (:data instance) (:data @instance)))
 
@@ -118,10 +120,12 @@
       (update-in instance [:data] (fn [_] (apply dissoc data keywords)))
       (swap! instance #(update-in % [:data] (fn [_] (apply dissoc data keywords)))))))
 
+
+;; Higher order functions
 (defn move 
   "Makes attemp to move machine instance to next state. Instance
   input has to be clojure.lang.Atom
-  
+
   First transition function is called and input value for 
   transition function is instance itself. So during transition it is possible
   to change data in this instance. If there is no direct transition function
@@ -130,15 +134,15 @@
   When STM instance is moving \"from-state\" to \"to-state\" it will 
   make call for transition from state :any to \"to-state\" with value of
   @instance before transition. 
-  
+
   Last step of moving \"from-state\" to  \"to-state\" is calling transition
   from \"from-state\" to :any state with value of @instance before transition.
-  
+
   Return value is changed instance"
   [instance to-state]
   (if-let [stm (get-stm instance)]
     (do
-      (assert (contains? stm to-state) (str "STM doesn't contain " to-state "state"))
+      (assert (contains? stm to-state) (str "STM doesn't contain " to-state " state"))
       (if (valid-transition? instance (get-state instance) to-state)
         (let [from-state (get-state instance) 
               tfunction (get-transition stm from-state to-state)
@@ -159,4 +163,104 @@
               (swap! instance assoc :state to-state)
               instance)))))
     (assert false "There is no state machine configured for this instance")))
-    ;;(throw (Exception. "There is no state machine configured for this instance."))))
+
+
+
+(defn get-choices 
+  "Returns reachable states from current state 
+  of state machine instance in form of vector.
+
+  First directly reachable states are calculated
+  and set as first choices. If there are states in
+  STM of this instance that are reachable from
+  :any state than that choices are also valid
+  and are inserted after direct transitions."
+  ([x] (get-choices x (get-state x)))
+  ([x state]
+   (if-let [fix-choices (-> (if (map? x) x @x) :life state)]
+     ;; If choices are fixed than return all valid choices
+     (-> (filter #(contains? 
+                    (set (into (-> (get-transitions (get-stm x) :any) keys vec)
+                               (-> (get-transitions (get-stm x) state) keys vec)))
+                    %) fix-choices) vec)
+     (let [direct-transitions (-> (get-transitions (get-stm x) state) keys vec)
+           available? (fn [x y] (if (= -1 (.indexOf x y)) false true))
+           any-transitions (-> (get-transitions (get-stm x) :any) keys vec)
+           sum-transitions (reduce (fn [x y] (if-not (available? x y) (conj x y) x)) direct-transitions any-transitions)]
+       sum-transitions))))
+
+
+;; State machine life and behaviour
+(defn give-life!
+  "Give STM instance life... If no choices
+  are given than instance is free to live on
+  its own. If there are choices that restrict
+  machine behaviour than that rules are applied.
+
+  Choices are supposed to be map in form:
+
+  {:state1 [:state2 :state3 :state1]}
+
+  This means that if machine is in :state1 first
+  choice is :state2, second choice is :state3 etc." 
+  ([x] (give-life! x nil))
+  ([x choices] (if (map? x) 
+                 (assoc x :alive? true :life choices :last-choice nil :last-state nil)
+                 (do 
+                   (swap! x assoc :alive? true :life choices :last-choice nil :last-state nil)
+                   x))))
+
+(defn kill! [x]
+  (if (map? x) 
+    (assoc x :alive false)
+    (do
+      (swap! x assoc :alive false)
+      x)))
+
+;;(defn ^:private remove-element [sequence idx] 
+;;  (vec (concat (subvec sequence 0 idx) (when (>= idx 0) (subvec sequence (inc idx))))))
+;;
+;;(defn ^:private  move-to-last-position [sequence value]
+;;  (let [position (.indexOf sequence value)]
+;;    (if (= -1 position)
+;;      sequence
+;;      (conj (remove-element sequence position) value))))
+;;
+;;(defn remove-choice [sequence value]
+;;  (let [position (.indexOf sequence value)]
+;;    (if (= -1 position)
+;;      sequence
+;;      (remove-element sequence position))))
+
+(defn ^:private move-to-next-choice [x next-choice]
+  (let [state (get-state x)
+        last-choice (:last-choice x)
+        x (move x next-choice)]
+    (if (= (get-state x) next-choice)
+      (if (map? x)
+        (assoc x :last-state state :last-choice nil)
+        (do (swap! x assoc :last-state state :last-choice nil) x))
+      (if (map? x)
+        (assoc x :last-choice next-choice)
+        (do (swap! x assoc :last-choice next-choice) x)))))
+
+
+(defn act! 
+  "Moves state machine to next state. If transition
+  priority is defined with give-life! function than
+  that rules are applied. Otherwise state machine 
+  acts free of will...
+  
+  Free will can be :clockwise or "
+  ([x] (act! x :clockwise))
+  ([x m-character]
+    (if-let [instance (if (map? x) x @x)]
+      (do
+        (assert (:alive? instance) "Instance is not alive! First give it life...")
+        (let [available-choices (get-choices instance)]
+          (case m-character
+            :clockwise (move-to-next-choice x (available-choices (-> (.indexOf available-choices (or (:last-choice instance) (:last-state instance) -1))
+                                                                     inc
+                                                                     (rem (count available-choices)))))
+            :random (move-to-next-choice x (available-choices (-> available-choices count rand int))))))
+      (assert false "This is not STM instance"))))
